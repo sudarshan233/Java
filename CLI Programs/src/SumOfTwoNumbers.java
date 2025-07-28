8d9a3333188c31596d0d4cab578c29b490494058
sudarshan233 <nsridharsudarshan@gmail.com>
2025-07-28 13:47:18 +0530

    The java program to calculate sum of two numbers given as command-line arguments.

---
+++ CLI Programs/src/SumOfTwoNumbers.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+    }
+}
